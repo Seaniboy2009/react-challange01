6c8aadaa6e3ca81acb85e279b8c466fbd1692d8b
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-02-03 13:55:27 +0000

    Initial commit

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function SideBar() {
+    return <div>
+                <a>MyPhotos</a>
+                <a>My Illustrations</a>
+                <a>My Paintings</a>
+            </div>
+
+}
+
+export default SideBar;
