f5e44c3d429b1a5277ebefea244c6f193b3a8eac
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-02-04 11:11:04 +0000

    added navbar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
 
 function SideBar() {
-    return <div>
+    return <div className={css.sidebar}>
                 <a>MyPhotos</a>
                 <a>My Illustrations</a>
                 <a>My Paintings</a>
