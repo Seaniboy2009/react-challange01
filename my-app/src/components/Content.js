92b222c2efe1f659ee9e47812b32f0b185810923
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-02-16 10:34:31 +0000

    add search function for author

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -6,20 +6,49 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
+          posts: []
 
         }
     }
+
+    componentDidMount() {
+      console.log("Component did mount")
+      this.setState({
+        posts: savedPosts
+      })
+    }
+
+    handleEvent = (event) => {
+      const name = event.target.value
+      const filteredPosts = savedPosts.filter(post => {
+        return post.name.toLowerCase().includes(name)
+      })
+      this.setState({
+        posts: filteredPosts,
+      })
+      console.log(filteredPosts)
+
+    }
+
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label for="searchInput">Search:</label>
+              <input onChange={(event) => this.handleEvent(event)}
+                    id="searchInput" 
+                    type="search"
+                    placeholder="By Author"></input>
+              <h4>Posts found: {this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
-            {savedPosts.map(post => {
+            {this.state.posts.map(post => {
                 return <div key={post.title} className={css.SearchItem}>
-                            <p>{post.title}</p>
-                            <p>{post.name}</p>
+                            <p>Title: {post.title}</p>
+                            <p>Author: {post.name}</p>
                             <img src={post.image}></img>
                             <p>{post.description}</p>
 
